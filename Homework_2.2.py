0c42fc641a7dcdaeb5b96d2a426e61a3349c0db3
Homework 2.1 <primshyts1@gmail.com>
2025-03-04 11:22:36 +0100

    Homework 2.2

--- Homework_2.2.py
+++ Homework_2.2.py
@@ -1,7 +1,7 @@
 number = int(input("Please enter only 5 digits whole number to get inverse number: "))
-n1=number//10000 #1
-n2=number%10000//1000 #2
-n3=number%1000//100 # 3
+n1=number//10000
+n2=number%10000//1000
+n3=number%1000//100
 n4=number%100//10
 n5=number%10
 result=n5*10000+n4*1000+n3*100+n2*10+n1
