ca416dcf7126dd0a90470412774b95aba4fd8dc3
Homework 2.1 <primshyts1@gmail.com>
2025-03-04 11:30:37 +0100

    Homework 2.2

--- Homework_2.2.py
+++ Homework_2.2.py
@@ -5,4 +5,4 @@ n3=number%1000//100
 n4=number%100//10
 n5=number%10
 result=n5*10000+n4*1000+n3*100+n2*10+n1
-print("This is your inverse number:", result)
+print("This is your inverse number: ", result)
