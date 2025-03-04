1d1e4278fa28f9a2fdac39b3ed7d0b8acf0a348e
Homework 2.1 <primshyts1@gmail.com>
2025-03-04 11:51:15 +0100

    Homework 2.1

--- Homework_2.1.py
+++ Homework_2.1.py
@@ -1,11 +1,11 @@
-#V1
+#V.1
 number = int(input("Please enter only 4 digits whole number: "))
 n1=number//1000
 n2=number%1000//100
 n3=number%100//10
 n4=number%10
 print(n1,n2,n3,n4, sep='\n')
-#V2
+#V.2
 number = int(input("Please enter only 4 digits whole number: "))
 n1,rest1=divmod(number,1000)
 n2,rest2=divmod(rest1,100)
