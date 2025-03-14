3ef3b74cce11e8989eaec45d6df08b39897999fa
Homework 2.1 <primshyts1@gmail.com>
2025-03-14 16:54:53 +0100

    Homework_5.1

--- Homework_5.1.py
+++ Homework_5.1.py
@@ -1,8 +1,28 @@
 import keyword
 import string
+symbols = string.punctuation
+symbols2 = symbols.replace('_', '')
 your_variable = input('write name for variable and we will check it: ')
-print(your_variable.islower())
-print(not your_variable[0].isdigit())
-print(not your_variable == keyword.kwlist)
-print(your_variable == string.punctuation)
-print(not your_variable >= '_'*2)
+for i in range(5):
+    r1 = your_variable.islower()
+    r2 = not your_variable[0].isdigit()
+    r3 = not your_variable == keyword.kwlist
+    r4 = your_variable == symbols2
+    r5 = not your_variable >= '_'*2
+    if r1 == False:
+        print(False)
+        break
+    if r2 == False:
+        print(False)
+        break
+    if r3 == False:
+        print(False)
+        break
+    if r4 == False:
+        print(False)
+        break
+    if r5 == False:
+        print(False)
+        break
+    else:
+        print(True)
