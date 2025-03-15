e38f6755a2c42ea4c4f9e6bb5e3598ea4a7cb4d6
Homework 2.1 <primshyts1@gmail.com>
2025-03-15 10:47:08 +0100

    Homework_5.1

--- Homework_5.1.py
+++ Homework_5.1.py
@@ -1,28 +1,17 @@
 import keyword
 import string
-symbols = string.punctuation
-symbols2 = symbols.replace('_', '')
 your_variable = input('write name for variable and we will check it: ')
-for i in range(5):
+for i in range(1):
+    symbols = string.punctuation
+    symbols2 = symbols.replace('_', '')
+    count_symbol = your_variable.count("_")
+    check = 0
     r1 = your_variable.islower()
     r2 = not your_variable[0].isdigit()
     r3 = not your_variable == keyword.kwlist
-    r4 = your_variable == symbols2
-    r5 = not your_variable >= '_'*2
-    if r1 == False:
-        print(False)
-        break
-    if r2 == False:
-        print(False)
-        break
-    if r3 == False:
-        print(False)
-        break
-    if r4 == False:
-        print(False)
-        break
-    if r5 == False:
-        print(False)
-        break
-    else:
+    r4 = not your_variable.index(your_variable) == symbols2
+    r5 = not count_symbol >= 2
+    if r1 and r2 and r3 and r4 and r5 == True:
         print(True)
+    else:
+        print(False)
