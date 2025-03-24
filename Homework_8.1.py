7aa67bc561ad0521813be8aa690fb507e777c910
Homework 2.1 <primshyts1@gmail.com>
2025-03-24 20:19:43 +0100

    Homework_8.1

--- Homework_8.1.py
+++ Homework_8.1.py
@@ -14,4 +14,3 @@ print(add_one([3, 5, 6, 6, 7, 3, 8]))
 assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'test1'
 assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'test2'
 assert add_one([3, 5, 6, 6, 7, 3, 8]) == [3, 5, 6, 6, 7, 3, 9], 'test3'
-
