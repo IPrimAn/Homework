a6f6e83beb9831bb2c6fe5aad22cc53afe1f8cc7
Homework 2.1 <primshyts1@gmail.com>
2025-03-24 20:16:28 +0100

    Homework_8.1

---
+++ Homework_8.1.py
@@ -0,0 +1,17 @@
+def add_one(some_list):
+    num = ""
+    for digit in some_list:
+        num += str(digit)
+    number = int(num) + 1
+    result = []
+    for digit in str(number):
+        result.append(int(digit))
+    return result
+print(add_one([1, 2, 3, 4]))
+print(add_one([9, 9, 9]))
+print(add_one([3, 5, 6, 6, 7, 3, 8]))
+###
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'test1'
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'test2'
+assert add_one([3, 5, 6, 6, 7, 3, 8]) == [3, 5, 6, 6, 7, 3, 9], 'test3'
+
