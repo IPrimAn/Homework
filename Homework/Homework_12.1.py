d54dbff258a997389c09345df9308e06e8b0e777
Homework 2.1 <primshyts1@gmail.com>
2025-04-19 16:57:54 +0200

    Homework_12.1

---
+++ Homework/Homework_12.1.py
@@ -0,0 +1,17 @@
+import codecs
+import re
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html = file.read()
+
+    cleaned_text = re.sub(r'<.*?>', '', html)
+    lines = cleaned_text.splitlines()
+
+    non_empty_lines = [line.strip() for line in lines if line.strip()]
+    result_text = '\n'.join(non_empty_lines)
+
+    with codecs.open(result_file, 'w', 'utf-8') as file:
+        file.write(result_text)
+
+delete_html_tags('draft.html')
